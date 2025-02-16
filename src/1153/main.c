b963b2a76a2ed576e6bf6831587592a623f66b0d
Igor Gabriel <igorgabrielsr@hotmail.com>
2025-02-16 16:16:14 -0300

    feat: solve problem 1153 on Beecrowd

---
+++ src/1153/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int factorial(int n) { return n == 1 ? 1 : factorial(n - 1) * n; }
+
+int main() {
+  int n;
+  scanf("%d", &n);
+  printf("%d\n", factorial(n));
+  return 0;
+}
